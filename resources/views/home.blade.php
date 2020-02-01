7c3c1c9796473df324136fee29ae9a038ae06c0a
Dlnvta <novitadela97@gmail.com>
2020-02-01 12:55:05 -0900

    git init

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('main')
+
+@section('title', 'My Home')
+
+@section('container')
+<div class="container">
+  <div class="row">
+      <div class="col-10">
+        <h1 class="mt-3">Annyeonghaseyo!</h1>
+      </div>
+  </div>
+</div>
+@endsection
+    
